215728130979a338276232e450e83f140232737e
devArtoria <contact@artoria.us>
2018-05-15 17:18:56 +0900

    ADD: data loader
    data loader that return tuple with shffled x_data and y_data

---
+++ load_data.py
@@ -0,0 +1,22 @@
+import numpy as np
+import random as rd
+
+x = np.load('./signlang-digits/Sign-language-digits-dataset/X.npy')
+y = np.load('./signlang-digits/Sign-language-digits-dataset/Y.npy')
+
+
+def load_signlang(size=2062):
+    x_data = list()
+    y_data = list()
+    size = size
+    for image, lable in zip(x, y):
+        if size is 1:
+            rd.shuffle(x_data)
+            rd.shuffle(y_data)
+            return (x_data, y_data)
+        x_data.append(image)
+        y_data.append(lable)
+        size -= 1
+
+a = load_signlang()
+print(a[1][1])
